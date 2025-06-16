f277de3ee29f03ec34a80bc6c6090496e48961ee
Sigmabond01 <smdnoor4966@gmail.com>
2025-06-16 14:47:27 +0530

    initialisation

---
+++ MangaVault/src/components/hero.tsx
@@ -0,0 +1,8 @@
+export const Hero = () => {
+    return <div className="flex justify-center items-center">
+        <div>
+        <h1 className="font-bold">MangaVault</h1>
+        <h1>Not every panel makes history. These did.</h1>
+        </div>
+    </div>
+}
