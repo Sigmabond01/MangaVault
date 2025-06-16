8f94f302288536a87d80609119a2b720dda4e8d8
Sigmabond01 <smdnoor4966@gmail.com>
2025-06-16 15:47:55 +0530

    added logo

---
+++ MangaVault/src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export const Logo = () => {
+    return <div className="absolute top-0 left-6">
+        <img src="/src/assets/logo.png" alt="Logo" className="w-32 h-32" />
+      </div>
+}
--- MangaVault/src/components/hero.tsx
+++ MangaVault/src/components/hero.tsx
@@ -1,8 +1,13 @@
+import { Logo } from "./Logo"
+
 export const Hero = () => {
-    return <div className="flex justify-center items-center">
-        <div>
-        <h1 className="font-bold">MangaVault</h1>
-        <h1>Not every panel makes history. These did.</h1>
-        </div>
+  return (
+    <div className="flex justify-center items-center bg-[url('/src/assets/narutobg.jpg')] bg-cover bg-center h-screen w-full">
+        <Logo />
+      <div className="text-center space-y-4">
+        <h1 className="font-bold text-5xl">MangaVault</h1>
+        <h2 className="text-2xl">Not every panel makes history. These did.</h2>
+      </div>
     </div>
-}
+  );
+};
